33d5dbe26f18ac2c93e671cc5eed58002f6d0d21
nossonh <85635177+nossonh@users.noreply.github.com>
2022-03-30 15:36:08 -0400

    Add Progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,57 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+
+const ProgressBar = ({ value, size, color = COLORS.primary}) => {  
+  const CONTAINERS = {
+    large: LargeContainer,
+    medium: MediumContainer,
+    small: SmallContainer,
+  };
+
+  const Component = CONTAINERS[size]
+  const rightRadius = value == 100 ? 5 : value >= 99.8 ? 3 : 0;
+  return (
+    <Component>
+      <Progress 
+        style={{'--right-radius': `${rightRadius}px`}}
+        value={value}
+        color={color}
+        />
+      </Component>
+    );
 };
+
+const Container = styled.div`
+  width: 300px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+const LargeContainer = styled(Container)`
+  padding: 5px;
+  height: 28px;
+  border-radius: 8px;
+`
+const MediumContainer = styled(Container)`
+  padding: 0px;
+  height: 13px;
+  border-radius: 8px;
+`
+
+const SmallContainer = styled(Container)`
+  height: 7px;
+  border-radius: 12px;
+`
+
+const Progress = styled.div`
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  background-color:  ${props => props.color};
+  width: ${props => props.value}%;
+  height: 100%;
+  border-top-right-radius: var(--right-radius);
+  border-bottom-right-radius: var(--right-radius);;
+`
 
 export default ProgressBar;
