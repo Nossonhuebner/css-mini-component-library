a47cef9d8fe1739332b216c34f7f1a524f1f9a0d
Nossonhuebner <nossonhuebner@gmail.com>
2022-03-31 15:34:58 -0400

    Redo styles

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,28 +8,63 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
+
   return (
-    <StyledSelect value={value} onChange={onChange}>
-      {children}
-    </StyledSelect>
+    <Wrapper>
+      <Selectt value={value} onChange={onChange}>
+        {children}
+      </Selectt>
+      <Display>
+        <DisplayValue>{displayedValue}</DisplayValue>
+        <IconWrapper>
+          <Icon id='chevron-down'/>
+        </IconWrapper>
+      </Display>
+    </Wrapper>
   );
 };
 
-const StyledSelect = styled.select`
-  padding-right: 37px;
-  border-right: 15px solid transparent;
-  border-left: 17px solid transparent;
-  border-top: 13px solid transparent;
-  border-bottom: 13px solid transparent;
-  border-radius: 8px;
-  background-color: ${COLORS.transparentGray15};
-  color: ${COLORS.gray700};
-
-
-  &:hover {
-    color: ${COLORS.black}
-  }
+const Wrapper = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: max-content;
 `
 
+const IconWrapper = styled.div`
+ position: absolute;
+ top: 7px;
+ right: 0;
+ bottom: 0;
+`
+
+const Display = styled.div`
+    pointer-events: none;
+    border: 1px solid black;
+    position: relative;
+    padding: 10px;
+    padding-right: 35px;
+    margin-right: 100px;
+`
+
+const DisplayValue = styled.div`
+
+`
+
+const Selectt = styled.select`
+    color: transparent;
+    border: none;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    padding: 0;
+    margin: 0;
+    background: transparent;
+    max-width: min-content;
+    &:focus {
+      outline: none;
+    }
+`
 
 export default Select;
